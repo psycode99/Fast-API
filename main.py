be560dd645a44caea83575804269c13271156779
Favour <georgefavour694@gmail.com>
2024-04-16 11:43:29 +0100

    create_posts

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+from random import random, randrange
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+my_posts = [{'title': 'Beaches in Florida', 'content': 'Check these awesome beaches now!', 'published': True, 'rating': 4, 'id':1},
+             {'title': 'Beaches in California', 'content': 'Check these awesome beaches now!', 'published': True, 'rating': 4, 'id':2},
+               {'title': 'Beaches in Spain', 'content': 'Check these awesome beaches now!', 'published': True, 'rating': 4, 'id':3}]
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get('/')
 async def root():
@@ -9,12 +22,14 @@ async def root():
 
 @app.get('/posts')
 def get_posts():
-    return {"data":"new post"}
+    return {"data":my_posts}
 
-@app.post('/createpost')
-def create_post(payLoad: dict = Body(...)):
-    print(payLoad)
-    return {"new_post": f"title: {payLoad['title']}, content: {payLoad['content']}"}
+@app.post('/posts')
+def create_posts(post: Post):
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
 
 
 
