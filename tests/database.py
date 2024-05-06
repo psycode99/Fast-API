a56474d65719b6ee03223444904579ef9fbf7ae2
Favour <georgefavour694@gmail.com>
2024-05-06 21:06:46 +0100

    test login route

---
+++ tests/database.py
@@ -0,0 +1,50 @@
+from fastapi.testclient import TestClient
+import pytest
+from app.main import app
+from app import schemas
+from app.config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.database import get_db, Base
+
+SQLACHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+
+engine = create_engine(SQLACHEMY_DATABASE_URL, pool_pre_ping=True)
+
+TestSessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+# sessionLocal serves as a factory for creating new sessions
+
+
+@pytest.fixture(scope="function")
+def session():
+    """
+    it deletes the previous tables when a new test is started
+    creates the new database tables before our code is run
+    and yields back a TestClient object
+    
+    """
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    """
+    this creates a new session via the TestSessionLocal
+    and stores it in the db variable and then yields this db variable back.
+    We can then use the new session via the db variable to query the database.
+    """
+    db = TestSessionLocal() 
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture(scope="function")
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app=app)
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,54 +1,19 @@
-from fastapi.testclient import TestClient
-import pytest
-from app.main import app
 from app import schemas
-from app.config import settings
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from app.database import get_db, Base
-
-SQLACHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
-
-engine = create_engine(SQLACHEMY_DATABASE_URL, pool_pre_ping=True)
-
-TestSessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-# sessionLocal serves as a factory for creating new sessions
+from .database import client, session
+import pytest
+from jose import jwt
+from app.oauth2 import SECRET_KEY, ALGORITHM
 
 
 @pytest.fixture
-def session():
-    """
-    it deletes the previous tables when a new test is started
-    creates the new database tables before our code is run
-    and yields back a TestClient object
-    
-    """
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
+def test_user(client):
+    user_data = {"email":"meeka@gmail.com", "password":"1234"}
+    res = client.post('/users/', json=user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
 
-    """
-    this creates a new session via the TestSessionLocal
-    and stores it in the db variable and then yields this db variable back.
-    We can then use the new session via the db variable to query the database.
-    """
-    db = TestSessionLocal() 
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@pytest.fixture
-def client(session):
-    def override_get_db():
-        try:
-            yield session
-        finally:
-            session.close()
-
-    app.dependency_overrides[get_db] = override_get_db
-    yield TestClient(app=app)
-    
 
 def test_root(client):
     res = client.get('/')
@@ -61,3 +26,13 @@ def test_create_user(client):
     new_user = schemas.UserResp(**res.json())
     assert new_user.email == "meeka@gmail.com"
     assert res.status_code == 201
+
+
+def test_login(client, test_user):
+    res = client.post('/login', data={"username":test_user['email'], "password":test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, SECRET_KEY, algorithms=[ALGORITHM])
+    id: str = payload.get('user_id')
+    assert id == test_user['id']
+    assert login_res.token_type == 'bearer'
+    assert res.status_code == 200
