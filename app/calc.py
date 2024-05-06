212beacb66f977578ef62e2ef63f82a89d066121
Favour <georgefavour694@gmail.com>
2024-05-06 13:31:49 +0100

    class tests

--- app/calc.py
+++ app/calc.py
@@ -1,2 +1,15 @@
 def add(x:int, y:int) -> int:
     return x+y
+
+class BankAccount():
+    def __init__(self, starting_balance=0) -> None:
+        self.balance = starting_balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        self.balance -= amount
+
+    def collect_interest(self):
+        self.balance *= 1.1
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,7 +1,36 @@
-from app.calc import add
+from app.calc import add, BankAccount
 import pytest
+
+@pytest.fixture
+def class_instance():
+    bank_acct = BankAccount(50)
+    return bank_acct
 
 @pytest.mark.parametrize("x, y, result", [(1,2,3), (2,2,4), (3,3,6)])
 def test_add(x, y, result):
     sum = add(x, y)
     assert sum == result
+
+
+def test_bank_set_inital_amt(class_instance):
+    assert class_instance.balance == 50
+
+
+def test_bank_default_initial_amt():
+    bank_acct = BankAccount()
+    assert bank_acct.balance == 0
+
+
+def test_bank_deposit(class_instance):
+    class_instance.deposit(50)
+    assert class_instance.balance == 100
+
+
+def test_bank_withdraw(class_instance):
+    class_instance.withdraw(30)
+    assert class_instance.balance == 20
+
+
+def test_bank_interest(class_instance):
+    class_instance.collect_interest()
+    assert round(class_instance.balance, 2) == 55
